9cb665a6a4eb4a5c9dd1bb3254e79f376165e7cc
zaebee <zaebuntu@gmail.com>
2012-01-29 23:21:41 +0400

    fix config.xml and settings

--- apps/dynamic_models/settings.py
+++ apps/dynamic_models/settings.py
@@ -5,3 +5,5 @@ import os
 APP_PATH = os.path.abspath(os.path.dirname(__file__))
 
 CONFIG_FILE = getattr(settings, 'DYNAMIC_CONFIG_FILE', os.path.join(APP_PATH, "config.xml"))
+MAX_LENGHT_CHAR = getattr(settings, 'DYNAMIC_MAX_LENGHT_CHAR', 80)
+MAX_LENGHT_INT = getattr(settings, 'DYNAMIC_MAX_LENGHT_INT', 5)
--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,41 @@
-/* Foundation v2.1.4 http://foundation.zurb.com */
+$.ajaxSetup({
+  beforeSend: function (request) {
+      request.setRequestHeader('X-CSRFToken',getCookie('csrftoken'));
+  },
+});
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 $(document).ready(function () {
 
 	function activateTab($tab) {
 		var $activeTab = $tab.closest('dl').find('a.active'),
-				contentLocation = $tab.attr("href") + 'Tab';
+				//contentLocation = $tab.attr("href") + 'Tab';
+				contentLocation = $tab.attr("href");
+        $.post(contentLocation, function(data){
+        });
 
 		//Make Tab Active
 		$activeTab.removeClass('active');
 		$tab.addClass('active');
 
     	//Show Tab Content
-		$(contentLocation).closest('.tabs-content').children('li').hide();
-		$(contentLocation).show();
+		//$(contentLocation).closest('.tabs-content').children('li').hide();
+		//$(contentLocation).show();
 	}
 
 	$('dl.tabs').each(function () {
@@ -19,6 +43,7 @@ $(document).ready(function () {
 		var tabs = $(this).children('dd').children('a');
 		tabs.click(function (e) {
 			activateTab($(this));
+      return false;
 		});
 	});
 
