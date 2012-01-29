e8093045bc2e83795fe96604cdec7aa5d71065ce
zaebee <zaebuntu@gmail.com>
2012-01-29 23:26:42 +0400

    added dynamic_models tempaltetags

---
+++ apps/dynamic_models/templatetags/dynamic_tags.py
@@ -0,0 +1,45 @@
+from django import template
+from dynamic_models.models import BaseDynamicModel
+
+register = template.Library()
+baseDynamic = BaseDynamicModel()
+
+
+@register.inclusion_tag('dynamic_models/tags/dummy.html', takes_context=True)
+def dynamic_tabs(context, template='dynamic_models/tags/dynamic_tabs.html'):
+    """Return links to event categories"""
+    models = {}
+    for model_name, model in baseDynamic.registry.items():
+        models.update({model_name: model._meta.verbose_name})
+
+    return {'template': template,
+            'models': models,
+            }
+
+
+class VerbatimNode(template.Node):
+
+    def __init__(self, text):
+        self.text = text
+
+    def render(self, context):
+        return self.text
+
+
+@register.tag
+def verbatim(parser, token):
+    text = []
+    while 1:
+        token = parser.tokens.pop(0)
+        if token.contents == 'endverbatim':
+            break
+        if token.token_type == template.TOKEN_VAR:
+            text.append('{{')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('{%')
+        text.append(token.contents)
+        if token.token_type == template.TOKEN_VAR:
+            text.append('}}')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('%}')
+    return VerbatimNode(''.join(text))
