51e5b4bd84604c4ed8fe8e1b05153d9966ddcf5d
zaebee <zaebuntu@gmail.com>
2012-01-29 23:23:40 +0400

    added view on json response

--- apps/dynamic_models/views.py
+++ apps/dynamic_models/views.py
@@ -1,8 +1,7 @@
 from django.db.models import get_model
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views.generic.simple import direct_to_template
 
-from django.utils import simplejson
 from django.core.serializers import serialize
 
 
@@ -10,10 +9,14 @@ def get_qs(request, model_name):
     model = get_model('dynamic_models', model_name)
     qs = model.objects.all()
     data = serialize('json', qs)
-    return HttpResponse(simplejson.dumps(data),
-                        mimetype='application/javascript')
+    if request.is_ajax():
+        return HttpResponse(data, mimetype='application/json')
+    else:
+        raise Http404
 
 
 def model_list(request):
+    if request.is_ajax():
+        raise Http404
     return direct_to_template(request, 'dynamic_models/model_list.html',
                               {'data': 'data'})
